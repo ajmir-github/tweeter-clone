85009a513d11ec5a8b96539666367aa174042b3f
Ajmir Raziqi <ajmir.ng3@gmail.com>
2025-12-09 14:36:57 +0200

    activate the tailwindcss

--- app/(private)/page.tsx
+++ app/(private)/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Hello world!
+      </h1>
+    </div>
+  );
 }
